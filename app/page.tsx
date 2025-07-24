97b21ca90ddf4e760a5e85d8b6ef54076ee30dc2
Miguel Henandez <miguelhernand57@gmail.com>
2025-07-23 22:38:47 -0500

    add ui.shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1>Hola Ecommerce Ropa Deportiva</h1>
+      <Button variant="destructive">Click</Button>
     </div>
   );
 }
